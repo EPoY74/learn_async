68b5ba9608141cd88f6a04f00460b1e7b1196580
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-16 13:33:38 +0500

    Делаю асинхронность на генераторах

--- first_lesson_async1.py
+++ first_lesson_async1.py
@@ -6,18 +6,27 @@ def clock():
     time0 = round(time.time())
     while True:
         if (round(time.time()) - time0) % 5 == 0:
-            print("5 sec")
-            time.sleep(1)
-
+            # print("5 sec")
+            yield  "5 sec"
+        else:
+            yield 0 
 
 def query():
     for i in os.walk("c:\\"):
-        print(i[0])
+        # print(i[0])
+        # Далаем генератор
+        yield i[0]  
 
 
 def main():
-    print(query)
-    # query()
-    # clock()
+    data = query()
+    alarm = clock()
+    
+    while True:
+        d = next(data)
+        a = next(alarm)
+        print(d)
+        if a: print(a)
+        time.sleep(1)
 
 main()
