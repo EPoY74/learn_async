c31b4c76aef283df03f012137735fb42b60b7348
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-16 12:04:45 +0500

    Добавил комментарии к функциям

--- first_lesson.py
+++ first_lesson.py
@@ -6,18 +6,26 @@
 import time
 
 def print1():
+    """Выводим число 1
+    """
     print(1)
 
 def print2():
+    """Ожидаю и вывожу число 2
+    """
     print("I am sleeping")
     time.sleep(10)
     print(2)
 
 def print3():
+    """Выводим число 1
+    """
     print(3)
 
 
 def main():
+    """Функция для запуска всего кода
+    """
     print1()
     print2()
     print3()
