da1da9c6736a0fae06eca61ee953138147922ae0
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-16 09:38:34 +0500

    first commit

---
+++ first_lesson.py
@@ -0,0 +1,26 @@
+"""
+
+ИСточник урока:  https://www.youtube.com/watch?v=_4QY1nGFRY8&t=2156s
+"""
+
+import time
+
+def print1():
+    print(1)
+
+def print2():
+    print("I am sleeping")
+    time.sleep(10)
+    print(2)
+
+def print3():
+    print(3)
+
+
+def main():
+    print1()
+    print2()
+    print3()
+
+
+main()
