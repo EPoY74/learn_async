eda1026a82f8133c30e9ea20550cf1e6b751cd0e
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-16 15:01:09 +0500

    Асинхронный вызов функций. Версия 1. Работает.

--- first_lesson.py
+++ first_lesson.py
@@ -3,32 +3,37 @@
 ИСточник урока:  https://www.youtube.com/watch?v=_4QY1nGFRY8&t=2156s
 """
 
-import time
+import asyncio
 
-def print1():
+async def print1():
     """Выводим число 1
     """
     print(1)
 
-def print2():
+async def print2():
     """Ожидаю и вывожу число 2
     """
-    print("I am sleeping")
-    time.sleep(10)
-    print(2)
+    print("I am sleeping. I am num 2")
+    await asyncio.sleep(10)
+    print("I am num 2. I woke up!")
 
-def print3():
-    """Выводим число 1
+async def print3():
+    """Выводим число 3
     """
     print(3)
 
 
-def main():
+async def main():
     """Функция для запуска всего кода
     """
-    print1()
-    print2()
-    print3()
+    task1 = asyncio.create_task(print1())
+    task2 = asyncio.create_task(print2())
+    task3 = asyncio.create_task(print3())
+
+    await task1
+    await task2
+    await task3
 
 
-main()
+# Запускаю событийный цикл
+asyncio.run(main())
