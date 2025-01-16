aab881a234d85258ba9fdaed313441db9f2803c2
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-16 10:13:59 +0500

    Еще раз поправил импорты и код

--- first_lesson_tkinter.py
+++ first_lesson_tkinter.py
@@ -2,23 +2,25 @@
 ИСточник урока:  https://www.youtube.com/watch?v=_4QY1nGFRY8&t=2156s
 """
 
-import tkinter
+from tkinter import ttk
+import tkinter  
 import time
-import tkinter.ttk
 
 root = tkinter.Tk()
 root.geometry("300x300+150+150")
 
 def sleep_func():
+    """Функция имитирует блокирующую сущность
+    """
     time.sleep(10)
-    lab:dict={}
+    # lab:dict={}
     lab['text'] = "После сна"
 
 
-btn = tkinter.ttk.Button(root, text="Run", command = sleep_func)
+btn = ttk.Button(root, text="Run", command = sleep_func)
 btn.place(relx=0.5, rely=0.2, anchor=tkinter.CENTER)
 
-lab = tkinter.ttk.Label(root, text="Текст до нажатия")
+lab = ttk.Label(root, text="Текст до нажатия")
 lab.place(relx=0.5, rely=0.6, anchor=tkinter.CENTER)
 
 
