53f2837bded28ae50e11e59a7e429888d497103b
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-18 07:50:57 +0500

    Ввел дополнительную функцию

--- first_lesson_TaskGroup(16).py
+++ first_lesson_TaskGroup(16).py
@@ -1,9 +1,22 @@
 import asyncio
 import time
 
+
+async def do_something(sec:int) -> None:
+    """Смысл ввода этой функции - что бы показать, что при её вызове не надо
+    её также добавлять в группу задач. 
+
+    Args:
+        sec (int): Число. Берется из запускающнго цикла
+    """
+    await asyncio.sleep(sec)
+    print(f"result: {sec}")
+
+
 async def print1(sec:int):
     await asyncio.sleep(sec)
     print(sec)
+    await do_something(sec)
 
 
 async def main():
